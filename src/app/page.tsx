f75a2a0c233da7340244e1f621592b2de62677d2
DanielBGC <danielbgc02@gmail.com>
2023-03-25 01:51:55 -0300

    Realizando uma requisição HTTP dentro do corpo do próprio componente, sem criar uma função separada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch('https://api.github.com/users/danielbgc');
+  const user = await response.json();
+
   return (
     <div>
       <h1>Home</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
       <Link href='/dashboard'>Dashboard</Link>
     </div>
   );
