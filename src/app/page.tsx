b70cfa3c0c99d74158458b986236d4012d3c1f30
DanielBGC <danielbgc02@gmail.com>
2023-03-25 02:19:03 -0300

    Utilizando Promise.all caso uma página tenha que fazer diversas requisições HTTP independentes entre si

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
 import { User } from '@/components/User';
 import Link from 'next/link';
 
-export default function Home() {
+export default async function Home() {
+  const [user, repos] = await Promise.all([
+    fetch('https://api.github.com/users/danielbgc').then((response) =>
+      response.json()
+    ),
+    fetch('https://api.github.com/users/danielbgc/repos').then((response) =>
+      response.json()
+    ),
+  ]);
+
   return (
     <div>
       <h1>Home</h1>
 
-      {/* @ts-expect-error Async Server Component */}
-      <User />
+      <h2>User</h2>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+
+      <h2>Repos</h2>
+      <pre>{JSON.stringify(repos, null, 2)}</pre>
 
       <Link href='/dashboard'>Dashboard</Link>
     </div>
